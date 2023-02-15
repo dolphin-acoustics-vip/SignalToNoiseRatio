9e07cd0f57b7e6e5b04d588a30f74999bc7cb8a8
samina-bibi <91482270+samina-bibi@users.noreply.github.com>
2023-02-15 14:51:33 +0000

    added changes to displaying the spectrogram

--- src/main.m
+++ src/main.m
@@ -2,12 +2,24 @@
 % reading the wav file.
 [samples, fs] = audioread("test_files/20170728_031259_ch05_sel02.wav");
 
+reducedFreq = decimate(samples, 10);
+reducedFS = fs / 10;
+% make sure the sample rate is double the highest frequency
+% - dolphin whistles below 20khz
+% need to reduce the frequency of the data
+% 44.1khz standard stereo sampling rates - would be the sampling rate of
+% wav file - check
+% if the sample rate >500khz 
+
+
+% make a line to overlay over the contours.
 window = hamming(512);
-nfft = 2048;
-nooverlap = 256;
+nfft = 512;
+nooverlap = 128;
 
-spectrogram(samples, window, nooverlap, nfft, fs, 'yaxis')
-
+spectrogram(reducedFreq, window, nooverlap, nfft, reducedFS, 'yaxis')
+specVal = spectrogram(reducedFreq, window, nooverlap, nfft, reducedFS, 'yaxis');
+specVal = abs(specVal);
 disp(fs)
 
 % loading all of the contour stats into a matrix.
