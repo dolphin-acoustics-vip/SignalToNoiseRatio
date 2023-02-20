29947ece87d8204737152c3928cda114e3408061
samina-bibi <91482270+samina-bibi@users.noreply.github.com>
2023-02-20 14:42:33 +0000

    Methods for plotting csv/spectrogram given file
    need to edit plot csv to get the date stamp from the file name.

---
+++ src/plotSpectrogram.m
@@ -0,0 +1,26 @@
+% This file holds the method for plotting the spectrogram onto the GUI.
+
+function plotSpectrogram(file)
+    [samples, fs] = audioread(file);
+
+    % Decimating, so that the sample rate is double the highest frequency -
+    % Nyquist's Theorem.
+    reducedFreq = decimate(samples, 10);
+    reducedFS = fs / 10;
+    
+    % Arguments for the spectrogram function.
+    window = hamming(512);
+    nfft = 512;
+    nooverlap = 128;
+
+    % This usually opens in its own window, but how to have this go onto
+    % the GUI?
+    spectrogram(reducedFreq, window, nooverlap, nfft, reducedFS, 'yaxis')
+end
+
+% make sure the sample rate is double the highest frequency
+% - dolphin whistles below 20khz
+% need to reduce the frequency of the data
+% 44.1khz standard stereo sampling rates - would be the sampling rate of
+% wav file - check
+% if the sample rate >500khz 
