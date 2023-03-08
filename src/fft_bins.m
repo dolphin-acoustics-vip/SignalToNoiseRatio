86833ed0d9ffe52ee11d2d3103667702f56d5016
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2023-03-08 15:05:59 +0000

    Rewrote time bin code in terms of FFT Advance

--- src/fft_bins.m
+++ src/fft_bins.m
@@ -1,15 +1,12 @@
-function [specBinFreq, specBinTime] = sortFftBins(whistle, nfft, fs, numFreqBins, numTimeBins)
+function [specBinFreq, specBinTime] = sortFftBins(whistle, nfft, overlap, fs, numFreqBins)
     syms specBinFreq specBinTime;
     whistle_table = readtable(whistle);
     time = whistle_table.("Time_ms_");
     frequency = whistle_table.("PeakFrequency_Hz_");
-    
+
     min_time = min(time);
-    max_time = max(time);
-
-    min_frequency = min(frequency);
-    max_frequency = max(frequency);
-
+    advance = nfft - overlap;
+    
     specBinFreq = floor(frequency * numFreqBins / fs + 1);
-    specBinTime = floor((time - min_time) * numTimeBins /(max_time - min_time) + 1);
+    specBinTime = floor((time - min_time) * fs / advance + 1);
 end
